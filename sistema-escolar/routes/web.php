a909e830acf763e73874d7de7f475512471cb863
Saul Lainez <saullainez@hotmail.es>
2018-03-30 11:45:06 -0600

    Rutas y controladores

--- sistema-escolar/routes/web.php
+++ sistema-escolar/routes/web.php
@@ -14,3 +14,28 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('alumno', 'AlumnoController');
+Route::resource('asignaturacarrera', 'AsignaturaCarreraController');
+Route::resource('asignatura', 'AsignaturaController');
+Route::resource('asignaturaseccion', 'AsignaturaSeccionController');
+Route::resource('aula', 'AulaController');
+Route::resource('carrera', 'CarreraController');
+Route::resource('cicloasignatura', 'CicloAsignaturaController');
+Route::resource('ciclolectivo', 'CicloLectivoController');
+Route::resource('docenteasignatura', 'DocenteAsignaturaController');
+Route::resource('docente', 'DocenteController');
+Route::resource('docenteespecialidad', 'DocenteEspecialidadController');
+Route::resource('edificio', 'EdificioController');
+Route::resource('especialidad', 'EspecialidadController');
+Route::resource('parcial', 'ParcialController');
+Route::resource('periodoacademico', 'PeriodoAcademicoController');
+Route::resource('persona', 'PersonaController');
+Route::resource('seccionalumno', 'SeccionAlumnoController');
+Route::resource('seccionalumnoparcial', 'SeccionAlumnoParcialController');
+Route::resource('seccion', 'SeccionController');
+Route::resource('trabajoalumnodetalle', 'TrabajoAlumnoDetalleController');
+Route::resource('trabajoasignaturaseccion', 'TrabajoAsignaturaSeccionController');
+Route::resource('trabajo', 'TrabajoController');
+
