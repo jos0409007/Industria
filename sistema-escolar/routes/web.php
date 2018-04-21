d84db4e9ed6a8e9eb407e3e47d8f252ba6f11016
Saul Lainez <saullainez@hotmail.es>
2018-04-20 22:30:27 -0600

    Instalacion AdminLTE

--- sistema-escolar/routes/web.php
+++ sistema-escolar/routes/web.php
@@ -12,30 +12,14 @@
 */
 
 Route::get('/', function () {
-    return view('prueba');
+    return view('welcome');
 });
 
+Route::group(['middleware' => 'auth'], function () {
+    //    Route::get('/link1', function ()    {
+//        // Uses Auth Middleware
+//    });
 
-Route::resource('alumno', 'AlumnoController');
-Route::resource('asignaturacarrera', 'AsignaturaCarreraController');
-Route::resource('asignatura', 'AsignaturaController');
-Route::resource('asignaturaseccion', 'AsignaturaSeccionController');
-Route::resource('aula', 'AulaController');
-Route::resource('carrera', 'CarreraController');
-Route::resource('cicloasignatura', 'CicloAsignaturaController');
-Route::resource('ciclolectivo', 'CicloLectivoController');
-Route::resource('docenteasignatura', 'DocenteAsignaturaController');
-Route::resource('docente', 'DocenteController');
-Route::resource('docenteespecialidad', 'DocenteEspecialidadController');
-Route::resource('edificio', 'EdificioController');
-Route::resource('especialidad', 'EspecialidadController');
-Route::resource('parcial', 'ParcialController');
-Route::resource('periodoacademico', 'PeriodoAcademicoController');
-Route::resource('persona', 'PersonaController');
-Route::resource('seccionalumno', 'SeccionAlumnoController');
-Route::resource('seccionalumnoparcial', 'SeccionAlumnoParcialController');
-Route::resource('seccion', 'SeccionController');
-Route::resource('trabajoalumnodetalle', 'TrabajoAlumnoDetalleController');
-Route::resource('trabajoasignaturaseccion', 'TrabajoAsignaturaSeccionController');
-Route::resource('trabajo', 'TrabajoController');
-
+    //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
+    #adminlte_routes
+});
