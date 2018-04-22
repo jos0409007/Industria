b7833d9e56ea85ec94f436577b22efee5dbc242a
Saul Lainez <saullainez@hotmail.es>
2018-04-22 01:18:11 -0600

    Actualizacion AdminLTE

--- sistema-escolar/resources/views/vendor/adminlte/home.blade.php
+++ sistema-escolar/resources/views/vendor/adminlte/home.blade.php
@@ -10,10 +10,9 @@
 		<div class="row">
 			<div class="col-md-8 col-md-offset-2">
 
-				<!-- Default box -->
-				<div class="box">
+				<!--<div class="box">
 					<div class="box-header with-border">
-						<h3 class="box-title">Home</h3>
+						<h3 class="box-title">Inicio</h3>
 
 						<div class="box-tools pull-right">
 							<button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
@@ -25,9 +24,8 @@
 					<div class="box-body">
 						{{ trans('adminlte_lang::message.logged') }}. Start creating your amazing application!
 					</div>
-					<!-- /.box-body -->
-				</div>
-				<!-- /.box -->
+				</div>-->
+
 
 			</div>
 		</div>
--- sistema-escolar/resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
+++ sistema-escolar/resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
@@ -18,30 +18,29 @@
             </div>
         @endif
 
-        <!-- search form (Optional) -->
-        <form action="#" method="get" class="sidebar-form">
+        <!--<form action="#" method="get" class="sidebar-form">
             <div class="input-group">
                 <input type="text" name="q" class="form-control" placeholder="{{ trans('adminlte_lang::message.search') }}..."/>
               <span class="input-group-btn">
                 <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i></button>
               </span>
             </div>
-        </form>
-        <!-- /.search form -->
+        </form>-->
 
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu" data-widget="tree">
-            <li class="header">{{ trans('adminlte_lang::message.header') }}</li>
+            <li class="header">Opciones</li>
             <!-- Optionally, you can add icons to the links -->
-            <li class="active"><a href="{{ url('home') }}"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.home') }}</span></a></li>
-            <li><a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.anotherlink') }}</span></a></li>
-            <li class="treeview">
+            <li class="active"><a href="{{ url('home') }}"><i class='fa fa-users'></i> <span>Usuarios</span></a></li>
+            <li class="active"><a href="{{ url('home') }}"><i class='fa fa-link'></i> <span>Roles</span></a></li>
+            <li class="active"><a href="{{ url('home') }}"><i class='fa fa-link'></i> <span>Permisos</span></a></li>
+            <!--<li class="treeview">
                 <a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.multilevel') }}</span> <i class="fa fa-angle-left pull-right"></i></a>
                 <ul class="treeview-menu">
                     <li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>
                     <li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>
                 </ul>
-            </li>
+            </li>->
         </ul><!-- /.sidebar-menu -->
     </section>
     <!-- /.sidebar -->
